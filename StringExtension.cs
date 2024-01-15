54e94b0e107936673b66cbf769d3f59a61b93e10
Evgenii Neretin <strtoid.zak2018@yandex.ru>
2024-01-15 15:13:44 +0300

    Add project files.

---
+++ StringExtension.cs
@@ -0,0 +1,58 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace EvnerLib
+{
+    /// <summary>
+    /// Класс расширений парсинга строки
+    /// </summary>
+    public static class StringExtension
+    {
+        /// <summary>
+        /// Запарсит дробное число из любых строк
+        /// </summary>
+        /// <remarks>95.21 теперь парсится!</remarks>
+        /// <param name="strToParse">Строка</param>
+        /// <param name="decimalSymbol"></param>
+        /// <returns></returns>
+        public static float DoubleParseAdvanced(this string strToParse, char decimalSymbol = ',')
+        {
+            string tmp;
+            try
+            {
+                tmp = Regex.Match(strToParse, @"([-]?[0-9]+)([\s])?([0-9]+)?[." + decimalSymbol + "]?([0-9 ]+)?([0-9]+)?").Value;
+
+
+                if (tmp.Length > 0 && strToParse.Contains(tmp))
+                {
+                    var currDecSeparator = CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+
+                    tmp = tmp.Replace(".", currDecSeparator).Replace(decimalSymbol.ToString(), currDecSeparator);
+
+                    return float.Parse(tmp);
+                }
+            }
+            catch (Exception ex)
+            {
+                tmp = "0";
+                return float.Parse(tmp);
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// Отобразит double число с точкой вместо запятой в строке
+        /// </summary>
+        /// <param name="Double"></param>
+        /// <returns></returns>
+        public static string ChangeSymbolInDoubleToString(this double Double)
+        {
+            var reminder = (Math.Round(Double - Math.Floor(Double), 2) * 100);
+
+            int Floor = (int)Math.Floor(Double);
+            string res = $"{Floor}.{reminder}";
+            return res;
+        }
+
+    }
+}
